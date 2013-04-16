410eea6e32335389fa632c5f57489d8cf7bc617d
Uri Laserson <laserson@cloudera.com>
2013-04-15 23:42:03 -0700

    Populated setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='squarify',
+    version='0.1',
+    packages=['squarify'],
+    author='Uri Laserson',
+    author_email='uri.laserson@gmail.com',
+    description='Pure Python implementation of the squarify treemap layout algorithm',
+    license='Apache license, version 2',
+    keywords='treemap visualization squarify layout graphics',
+    url='https://github.com/laserson/squarify'
+)
