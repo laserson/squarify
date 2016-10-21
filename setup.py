ffb9f90d9f8c8351f2bf8cbb4d17401a1976c2cb
Uri Laserson <uri.laserson@gmail.com>
2016-10-21 13:32:31 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='squarify',
-    version='0.1.3',
+    version='0.2.0',
     packages=['squarify'],
     author='Uri Laserson',
     author_email='uri.laserson@gmail.com',
