d28a137db485ac1775b0a6e537c1f8f9d0d20c02
Uri Laserson <uri.laserson@gmail.com>
2019-01-30 16:22:41 -0500

    Reformatted code with black

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='squarify',
-    version='0.3.0',
-    packages=['squarify'],
-    author='Uri Laserson',
-    author_email='uri.laserson@gmail.com',
-    description='Pure Python implementation of the squarify treemap layout algorithm',
-    license='Apache license, version 2',
-    keywords='treemap visualization squarify layout graphics',
-    url='https://github.com/laserson/squarify'
+    name="squarify",
+    version="0.3.0",
+    packages=["squarify"],
+    author="Uri Laserson",
+    author_email="uri.laserson@gmail.com",
+    description="Pure Python implementation of the squarify treemap layout algorithm",
+    license="Apache license, version 2",
+    keywords="treemap visualization squarify layout graphics",
+    url="https://github.com/laserson/squarify",
 )
--- squarify/__init__.py
+++ squarify/__init__.py
@@ -2,6 +2,7 @@
 # Implements algorithm from Bruls, Huizing, van Wijk, "Squarified Treemaps"
 #   (but not using their pseudocode)
 
+
 def normalize_sizes(sizes, dx, dy):
     total_size = sum(sizes)
     total_area = dx * dy
@@ -9,13 +10,15 @@ def normalize_sizes(sizes, dx, dy):
     sizes = map(lambda size: size * total_area / total_size, sizes)
     return list(sizes)
 
+
 def pad_rectangle(rect):
-    if rect['dx'] > 2:
-        rect['x'] += 1
-        rect['dx'] -= 2
-    if rect['dy'] > 2:
-        rect['y'] += 1
-        rect['dy'] -= 2
+    if rect["dx"] > 2:
+        rect["x"] += 1
+        rect["dx"] -= 2
+    if rect["dy"] > 2:
+        rect["y"] += 1
+        rect["dy"] -= 2
+
 
 def layoutrow(sizes, x, y, dx, dy):
     # generate rects for each size in sizes
@@ -26,9 +29,10 @@ def layoutrow(sizes, x, y, dx, dy):
     width = covered_area / dy
     rects = []
     for size in sizes:
-        rects.append({'x': x, 'y': y, 'dx': width, 'dy': size / width})
+        rects.append({"x": x, "y": y, "dx": width, "dy": size / width})
         y += size / width
     return rects
+
 
 def layoutcol(sizes, x, y, dx, dy):
     # generate rects for each size in sizes
@@ -39,12 +43,16 @@ def layoutcol(sizes, x, y, dx, dy):
     height = covered_area / dx
     rects = []
     for size in sizes:
-        rects.append({'x': x, 'y': y, 'dx': size / height, 'dy': height})
+        rects.append({"x": x, "y": y, "dx": size / height, "dy": height})
         x += size / height
     return rects
 
+
 def layout(sizes, x, y, dx, dy):
-    return layoutrow(sizes, x, y, dx, dy) if dx >= dy else layoutcol(sizes, x, y, dx, dy)
+    return (
+        layoutrow(sizes, x, y, dx, dy) if dx >= dy else layoutcol(sizes, x, y, dx, dy)
+    )
+
 
 def leftoverrow(sizes, x, y, dx, dy):
     # compute remaining area when dx >= dy
@@ -56,6 +64,7 @@ def leftoverrow(sizes, x, y, dx, dy):
     leftover_dy = dy
     return (leftover_x, leftover_y, leftover_dx, leftover_dy)
 
+
 def leftovercol(sizes, x, y, dx, dy):
     # compute remaining area when dx >= dy
     covered_area = sum(sizes)
@@ -66,11 +75,23 @@ def leftovercol(sizes, x, y, dx, dy):
     leftover_dy = dy - height
     return (leftover_x, leftover_y, leftover_dx, leftover_dy)
 
+
 def leftover(sizes, x, y, dx, dy):
-    return leftoverrow(sizes, x, y, dx, dy) if dx >= dy else leftovercol(sizes, x, y, dx, dy)
+    return (
+        leftoverrow(sizes, x, y, dx, dy)
+        if dx >= dy
+        else leftovercol(sizes, x, y, dx, dy)
+    )
+
 
 def worst_ratio(sizes, x, y, dx, dy):
-    return max([max(rect['dx'] / rect['dy'], rect['dy'] / rect['dx']) for rect in layout(sizes, x, y, dx, dy)])
+    return max(
+        [
+            max(rect["dx"] / rect["dy"], rect["dy"] / rect["dx"])
+            for rect in layout(sizes, x, y, dx, dy)
+        ]
+    )
+
 
 def squarify(sizes, x, y, dx, dy):
     # sizes should be pre-normalized wrt dx * dy (i.e., they should be same units)
@@ -86,14 +107,18 @@ def squarify(sizes, x, y, dx, dy):
 
     # figure out where 'split' should be
     i = 1
-    while i < len(sizes) and worst_ratio(sizes[:i], x, y, dx, dy) >= worst_ratio(sizes[:(i+1)], x, y, dx, dy):
+    while i < len(sizes) and worst_ratio(sizes[:i], x, y, dx, dy) >= worst_ratio(
+        sizes[: (i + 1)], x, y, dx, dy
+    ):
         i += 1
     current = sizes[:i]
     remaining = sizes[i:]
 
     (leftover_x, leftover_y, leftover_dx, leftover_dy) = leftover(current, x, y, dx, dy)
-    return layout(current, x, y, dx, dy) + \
-            squarify(remaining, leftover_x, leftover_y, leftover_dx, leftover_dy)
+    return layout(current, x, y, dx, dy) + squarify(
+        remaining, leftover_x, leftover_y, leftover_dx, leftover_dy
+    )
+
 
 def padded_squarify(sizes, x, y, dx, dy):
     rects = squarify(sizes, x, y, dx, dy)
@@ -101,9 +126,18 @@ def padded_squarify(sizes, x, y, dx, dy):
         pad_rectangle(rect)
     return rects
 
-def plot(sizes, norm_x=100, norm_y=100,
-         color=None, label=None, value=None,
-         ax=None, bar_kwargs=None, text_kwargs=None):
+
+def plot(
+    sizes,
+    norm_x=100,
+    norm_y=100,
+    color=None,
+    label=None,
+    value=None,
+    ax=None,
+    bar_kwargs=None,
+    text_kwargs=None,
+):
 
     """
     Plotting with Matplotlib.
@@ -133,6 +167,7 @@ def plot(sizes, norm_x=100, norm_y=100,
     if color is None:
         import matplotlib.cm
         import random
+
         cmap = matplotlib.cm.get_cmap()
         color = [cmap(random.random()) for i in range(len(sizes))]
 
@@ -144,26 +179,27 @@ def plot(sizes, norm_x=100, norm_y=100,
     normed = normalize_sizes(sizes, norm_x, norm_y)
     rects = squarify(normed, 0, 0, norm_x, norm_y)
 
-    x = [rect['x'] for rect in rects]
-    y = [rect['y'] for rect in rects]
-    dx = [rect['dx'] for rect in rects]
-    dy = [rect['dy'] for rect in rects]
+    x = [rect["x"] for rect in rects]
+    y = [rect["y"] for rect in rects]
+    dx = [rect["dx"] for rect in rects]
+    dy = [rect["dy"] for rect in rects]
 
-    ax.bar(x, dy, width=dx, bottom=y, color=color,
-        label=label, align='edge', **bar_kwargs)
+    ax.bar(
+        x, dy, width=dx, bottom=y, color=color, label=label, align="edge", **bar_kwargs
+    )
 
     if not value is None:
-        va = 'center' if label is None else 'top'
+        va = "center" if label is None else "top"
 
         for v, r in zip(value, rects):
-            x, y, dx, dy = r['x'], r['y'], r['dx'], r['dy']
-            ax.text(x + dx / 2, y + dy / 2, v, va=va, ha='center', **text_kwargs)
+            x, y, dx, dy = r["x"], r["y"], r["dx"], r["dy"]
+            ax.text(x + dx / 2, y + dy / 2, v, va=va, ha="center", **text_kwargs)
 
     if not label is None:
-        va = 'center' if value is None else 'bottom'
+        va = "center" if value is None else "bottom"
         for l, r in zip(label, rects):
-            x, y, dx, dy = r['x'], r['y'], r['dx'], r['dy']
-            ax.text(x + dx / 2, y + dy / 2, l, va=va, ha='center', **text_kwargs)
+            x, y, dx, dy = r["x"], r["y"], r["dx"], r["dy"]
+            ax.text(x + dx / 2, y + dy / 2, l, va=va, ha="center", **text_kwargs)
 
     ax.set_xlim(0, norm_x)
     ax.set_ylim(0, norm_y)
