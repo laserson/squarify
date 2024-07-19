f2147266b39e5fd13d8173af9fcc0cf0924a167f
Uri Laserson <u@snkt.st>
2024-07-19 14:48:41 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="squarify",
-    version="0.4.3",
+    version="0.4.4",
     packages=["squarify"],
     author="Uri Laserson",
     author_email="uri.laserson@gmail.com",
